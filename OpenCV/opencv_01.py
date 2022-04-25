9deeb3415a00e2275e55778d2597dfb156840d62
Asma-Rashidian <asma.rashidian7997@gmail.com>
2022-04-25 14:27:49 +0430

    requirments for first opencv code

--- OpenCV/opencv_01.py
+++ OpenCV/opencv_01.py
@@ -1,1 +1,27 @@
+"""
+    Our first script will cover basics image processing operations using images from movie .
+"""
 
+# import the necessary packages
+import argparse
+import imutils 
+import cv2
+from numpy import imag
+
+
+#Command Line Arguments 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True , help="image path")
+args= vars(ap.parse_args())
+
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth)
+image = cv2.imread(args["input"])
+(h, w,c) = image.shape
+print("Height : {} , Width : {} , Depth : {} ".format(h, w,c))
+
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Image" , image)
+cv2.waitKey(0)
