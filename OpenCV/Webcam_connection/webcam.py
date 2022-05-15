cea6de3049dd9da50cac9d2131cdff6de360291d
Asma-Rashidian <asma.rashidian7997@gmail.com>
2022-05-15 20:34:02 +0430

    the video capture device was set as 0

--- OpenCV/Webcam_connection/webcam.py
+++ OpenCV/Webcam_connection/webcam.py
@@ -2,7 +2,7 @@
 import cv2
 
 # Open the device at the ID 0
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 #Check whether user selected camera is opened successfully.
 if not (cap.isOpened()):
